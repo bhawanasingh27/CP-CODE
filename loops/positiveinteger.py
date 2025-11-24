d8b67a6cd202a26f50269e5b359b0701920f0c08
Bhawana Singh <bhawana.singh_bca24@gla.ac.in>
2025-11-24 09:38:34 +0530

    Add files via upload

---
+++ loops/positiveinteger.py
@@ -0,0 +1,6 @@
+# Write a program that takes a positive integer N as input from the user and prints all natural numbers numbers from 1 to N, with each number followed by a space.
+N = int(input("Enter a positive integer: "))
+
+for i in range(1, N + 1):
+    print(i, end=" ")
+
